73b3b7287fea88f0d043944b6cb6854d0416f7b0
Jonathan Higger <59105254+jjhiggz@users.noreply.github.com>
2023-01-22 12:13:56 -0700

    added escape and unescape'

---
+++ src/String/escape.test.ts
@@ -0,0 +1,29 @@
+import { expect, describe, it } from "vitest";
+import escape from "./escape.js";
+import unescape from "./unescape.js";
+
+describe("escape", function () {
+  let escaped = "&amp;&lt;&gt;&quot;&#39;/",
+    unescaped = "&<>\"'/";
+
+  escaped += escaped;
+  unescaped += unescaped;
+
+  it("should escape values", function () {
+    expect(escape(unescaped)).toBe(escaped);
+  });
+
+  it("should handle strings with nothing to escape", function () {
+    expect(escape("abc")).toBe("abc");
+  });
+
+  it("should escape the same characters unescaped by `_.unescape`", function () {
+    expect(escape(unescape(escaped))).toBe(escaped);
+  });
+
+  ["`", "/"].forEach(function (chr) {
+    it('should not escape the "' + chr + '" character', function () {
+      expect(escape(chr)).toBe(chr);
+    });
+  });
+});
---
+++ src/String/unescape.test.ts
@@ -0,0 +1,38 @@
+import { describe, expect, it } from "vitest";
+import unescape from "./unescape";
+import escape from "./escape";
+describe("unescape", function () {
+  var escaped = "&amp;&lt;&gt;&quot;&#39;/",
+    unescaped = "&<>\"'/";
+
+  escaped += escaped;
+  unescaped += unescaped;
+
+  it("should unescape entities in order", function () {
+    expect(unescape("&amp;lt;")).toBe("&lt;");
+  });
+
+  it("should unescape the proper entities", function () {
+    expect(unescape(escaped)).toBe(unescaped);
+  });
+
+  it("should handle strings with nothing to unescape", function () {
+    expect(unescape("abc")).toBe("abc");
+  });
+
+  it("should unescape the same characters escaped by `_.escape`", function () {
+    expect(unescape(escape(unescaped))).toBe(unescaped);
+  });
+
+  it("should handle leading zeros in html entities", function () {
+    expect(unescape("&#39;")).toBe("'");
+    expect(unescape("&#039;")).toBe("'");
+    expect(unescape("&#000039;")).toBe("'");
+  });
+
+  ["&#96;", "&#x2F;"].forEach(function (entity) {
+    it('should not unescape the "' + entity + '" entity', function () {
+      expect(unescape(entity)).toBe(entity);
+    });
+  });
+});
